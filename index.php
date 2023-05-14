5257471ebaee6d198e1b3d67af77027cf5e397f1
Pierre BLANCHARD <blanchard.pierre.78@gmail.com>
2023-05-14 20:53:37 +0200

    rcf-base(dossier img, dossier style, index.php, style.css, rcfBack.jpg)

---
+++ index.php
@@ -0,0 +1,43 @@
+<!-- INDEX PAGE -->
+<!-- ECF "Créer un site web avec HTML, CSS et Bootstrap", Par Pierre BLANCHARD -->
+<!-- Référence évaluation : GDWFSHTMLCSSBOOTEXAIII1A -->
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Réunion des campeurs de france Acceuil - Par Pierre BLANCHARD</title>
+    <meta name="description" content="Bienvenue sur le site de la réunion des campeurs de france, groupe de réunion de camping depuis 1995." >
+    <link rel="stylesheet" href="style/style.css">
+    <link 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" 
+        crossorigin="anonymous"
+    >
+
+</head>
+
+<body>
+
+    <main id="main">
+
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
